993c5942532d9916966904ea5347b194b91dcd86
MumbisDev <33399197+MumbisDev@users.noreply.github.com>
2025-05-14 23:12:41 -0400

    add a default prop for onDelete

--- react-vite/src/components/DeleteConfirmationModal/DeleteConfirmationModal.jsx
+++ react-vite/src/components/DeleteConfirmationModal/DeleteConfirmationModal.jsx
@@ -40,4 +40,8 @@ const DeleteConfirmationModal = ({ habit, onDelete }) => {
     );
 };
 
+DeleteConfirmationModal.defaultProps = {
+    onDelete: () => Promise.resolve(),
+};
+
 export default DeleteConfirmationModal;
