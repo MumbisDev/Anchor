a09aefd6a4fad3fe92035e23aa75098b79615fb6
MumbisDev <33399197+MumbisDev@users.noreply.github.com>
2025-05-31 14:15:40 -0400

    explaining function

--- react-vite/src/utils/utils.js
+++ react-vite/src/utils/utils.js
@@ -1,6 +1,7 @@
 // This is a utility file for string functions
 // utils.js
 
+// Capitalizes the first letter of a given string
 export const capitalizeFirstLetter = (string) => {
     if (!string) return '';
     return string.charAt(0).toUpperCase() + string.slice(1);
