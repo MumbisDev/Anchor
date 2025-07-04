0e33396fe7118f9e6f2bd45f8a7308d2ceef7a27
MumbisDev <33399197+MumbisDev@users.noreply.github.com>
2025-07-04 17:58:28 -0400

    Add comment for isAuthenticated variable in ProtectedRoute

--- react-vite/src/components/auth/ProtectedRoute.jsx
+++ react-vite/src/components/auth/ProtectedRoute.jsx
@@ -5,6 +5,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 // This component restricts access to routes based on user authentication status.
 const ProtectedRoute = () => {
     const user = useSelector(state => state.session.user);
+    // Boolean indicating if the user is authenticated
     const isAuthenticated = !!user; // Store authentication status
 
     if (!user) {
