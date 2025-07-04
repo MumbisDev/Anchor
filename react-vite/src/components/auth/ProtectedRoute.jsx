54a0aa8b784856f3fff00d68e4bb84b5e6cfc3e9
MumbisDev <33399197+MumbisDev@users.noreply.github.com>
2025-07-04 17:58:01 -0400

    Add import section comment to ProtectedRoute

--- react-vite/src/components/auth/ProtectedRoute.jsx
+++ react-vite/src/components/auth/ProtectedRoute.jsx
@@ -1,7 +1,8 @@
-// This component restricts access to routes based on user authentication status.
+// Import dependencies
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 
+// This component restricts access to routes based on user authentication status.
 const ProtectedRoute = () => {
     const user = useSelector(state => state.session.user);
     
